0a6c25c405e3c4bac1208c6c7251675cffb991c5
helissonomc <helissonomc@dcomp.ufs.br>
2020-03-20 19:42:16 -0300

    campos em updateestrela

--- app/Http/Controllers/CRUDEstrelaController/UEstrelaController.php
+++ app/Http/Controllers/CRUDEstrelaController/UEstrelaController.php
@@ -13,17 +13,48 @@ class UEstrelaController extends Controller
     public function updateresponse(Request $request)
     {   
       try{
+        if(strcasecmp($request->tipo,"Anã Vermelha") == 0){
+          $request->tipo = 1;
+        }else{
+            if(strcasecmp($request->tipo,"Anã Branca") == 0){
+                $request->tipo = 2;
+            }else{
+                if(strcasecmp($request->tipo,"Estrela Binária") == 0){
+                    $request->tipo = 3;
+                }else{
+                    if(strcasecmp($request->tipo,"Gigante Azul") == 0){
+                        $request->tipo = 4;
+                    }else{
+                        if(strcasecmp($request->tipo,"Gigante Vermelho") == 0){
+                            $request->tipo = 5;
+                        }
+                    }
+                }
+            }
+        }
+        if(strcasecmp($request->morreu,"Sim") == 0){
+            $request->morreu = 'true';
+        }else{
+            if(strcasecmp($request->morreu,"Não") == 0){
+                $request->morreu = 'false';
+            }
+        }
         $estrela =  DB::update('UPDATE estrela
     			    SET nome_estrela = ?,
     					tam_estrela = ?,
     					idade_estrela = ?,
-    					dist_terra_estrela = ?
+    					dist_terra_estrela = ?,
+              tipo = ?,
+              morte = ?
     			    WHERE id_estrela = ?',
     					[$request->nome_estrela,
                         $request->tam_estrela,
                         $request->idade_estrela,
                         $request->dist_terra_estrela,
-                        $request->id_estrela]);
+                        $request->tipo,
+                        $request->morreu,
+                        $request->id_estrela
+                        ]);
 
         if($estrela == 1){
           $msg = "Estrela de id: $request->id_estrela foi modificada com sucesso.";
